ea15cde8efdc59d9351bef6be7e687498836e378
Kawian <99052760@mydavinci.nl>
2021-09-14 12:10:20 +0200

    oop pokebattle werkt helemaal

--- index.php
+++ index.php
@@ -1,26 +1,19 @@
 <?php 
 
-spl_autoload_register(function ($class_name) {
-    $file_name = $class_name. '.php';
-    if (!file_exists($file_name)) {
-        return false;
-    }
-    include $file_name;
-});
+    include "Pokemon.php";
+    include "Pikachu.php";
+    include "Charmeleon.php";
+    include "EnergyType.php";
+    include "Attacks.php";
+    include "Weakness.php";
 
-// $pikachu = new Pikachu("karel");
-// echo "<pre>" . $pikachu->name ."</pre>";
-
-// $charmeleon = new Charmeleon("berend");
-// echo "<pre>" . $charmeleon->name . "</pre>";
-
-echo $pikachu->getName() . ' health : ' . $charmeleon->getHealth() . ' / ' . $pikachu->getHitPoints();
-	echo '<br>' . $pikachu->getName() . ' Attacks ' . $charmeleon->getName() . ' with a ' . $pikachu->attack(target: $charmeleon, attackNumber: 0) . ' attack ' . '<br>';
-	echo $pikachu->getName() . ' health : ' . $charmeleon->getHealth() . ' / ' . $pikachu->getHitPoints() . '<br>';
-
-	echo $charmeleon->getName() . ' health : ' . $pikachu->getHealth() . ' / ' . $charmeleon->getHitPoints();
-	echo '<br>' . $charmeleon->getName() . ' Attacks ' . $pikachu->getName() . ' with a ' . $charmeleon->attack(target: $pikachu, attackNumber: 0) . ' attack ' . '<br>';
-	echo $charmeleon->getName() . ' health : ' . $pikachu->getHealth() . ' / ' . $charmeleon->getHitPoints() . '<br>';
-
-	echo ' There are ' . Pokemon::getPopulation() . ' pokemon(s) alive! ';
+    $pikachu = new Pikachu('Pikachu');
+    print_r('<pre>'. $pikachu->name . '</pre>');
+    
+    $charmeleon = new Charmeleon('Charmeleon');
+    print_r('<pre>'. $charmeleon->name . '</pre>');
+    
+    
+    $charmeleon->battle($pikachu, $charmeleon->attacks['Flare']);
+    $pikachu->battle($charmeleon, $pikachu->attacks['Electric ring']);
 
